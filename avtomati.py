1c6588dd5ec0b9989c10c71c256ed7e71bfdec45
Tadej <tadej.c@gmail.com>
2014-10-10 23:41:32 +0200

    testno okolje z generatorjem nekonstantnih primerov in ogrodje za nekonstantne primere

--- avtomati.py
+++ avtomati.py
@@ -4,6 +4,8 @@ import sys
 import random
 
 testing=1
+testing_pulls=0
+testing_machines=[]
 
 class AvtomatiBase():
     def __init__(self, url="http://celtra-jackpot.com/1"):
@@ -11,7 +13,7 @@ class AvtomatiBase():
         self.pulls = 0  # koliko potegov smo ze izvedli
         self.nMachines = self.getNMachines()
         self.maxPulls = self.getPulls()
-        self.machines = [[i,0,0] for i in range(self.nMachines)]  # naredimo seznam oblike[[<zap st.>,<st. uspesnih potegov>,<st. vseh potegov>], <naslednji avtomat> ...]
+        self.machines = [[i,[]] for i in range(self.nMachines)]  # naredimo seznam oblike[[<zap st.>,[seznam vseh potegov]], <naslednji avtomat> ...]
 
     def start(self):
         pass
@@ -21,7 +23,7 @@ class AvtomatiBase():
         :return: stevilo potegov, ki so na voljo
         """
         if testing:
-            return 1000
+            return testing_pulls
         return int(urllib.urlopen(self.url+"/pulls").read())
 
     def getNMachines(self):
@@ -29,7 +31,7 @@ class AvtomatiBase():
         :return: stevilo avtomatov, ki so na voljo
         """
         if testing:
-            return 2
+            return len(testing_machines)
         return int(urllib.urlopen(self.url+"/machines").read())
 
     def pull(self, machine,iter=0):
@@ -40,13 +42,16 @@ class AvtomatiBase():
         """
         self.pulls += 1
         if testing:
-            chance=random.random()
-            if machine==1:
-                if chance<0.4:
-                    return 1
-            elif machine==2:
-                if chance<0.6:
-                    return 1
+            roll=random.random()
+            prev=0
+            for start, chance in testing_machines[machine-1]:
+                if self.pulls<start:
+                    prev=chance
+                else:
+                    if roll<chance:
+                        return 1
+                    else:
+                        return 0
             return 0
 
         url = "%s/%d/%d" % (self.url, machine, self.pulls)
@@ -68,15 +73,18 @@ class Avtomati(AvtomatiBase):
         self.start()
 
     def start(self):
+        #pozene algoritem
         for i in xrange(self.maxPulls):
             machine = max(self.machines, key=self.weight)
-            self.machines[machine[0]][2] += 1
-            if self.pull(machine[0]+1):
-                self.machines[machine[0]][1] += 1
+            ##self.machines[machine[0]][2] += 1
+            res=self.pull(machine[0]+1)
+            self.machines[machine[0]][1].append(res)
+                ##self.machines[machine[0]][1] += 1
 
     def weight(self,x):
         #TODO: ugibanje spremembe verjetnosti
-        n,successful,all=x
+        all=len(x[1])
+        successful=sum(x[1])
         if all==0:
             all=0.1
         success_rate = successful / all
@@ -93,14 +101,31 @@ def run():
     return a
 
 def test():
-    for i in range(10):
+    global testing,testing_pulls,testing_machines
+    testing=1
+    testing_pulls,testing_machines=makeTestCase()
+    for i in range(3):
         a=Avtomati("http://celtra-jackpot.com/%d" %(i+1,))
-        print a.maxPulls,a.nMachines
-        print [(s/al,s,al) for n,s,al in a.machines]
+        print "potegov:",a.maxPulls,", avtomatov:",a.nMachines
+        for (n,s),machine in zip(a.machines,testing_machines):
+            print "%5d %5d  %.3f   %s"%(sum(s),len(s),sum(s)/len(s),str(machine))
+        #print [(sum(s)/len(s),sum(s),len(s)) for n,s in a.machines]
+        #print testing_machines
+        print "uspesnih:", sum([sum(s) for n,s in a.machines])
         print
 
 
+def makeTestCase():
+    machines=[]
+    cases=random.randint(500,3000)
+    for i in range(random.randint(2,10)):
+        machines.append([])
+        n=random.randint(1,cases//200)
+        for j in range(n):
+            machines[-1].append([j*cases//n,random.random()])
+    return cases,machines
+
 if __name__ == "__main__":
-    test()
+    a=test()
 
 
